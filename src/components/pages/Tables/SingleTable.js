39348e8eb2259dc8be1b238e28973a38c330f65a
Konstanty Wrona <koswro@gmail.com>
2023-04-13 01:54:30 +0200

    Change SingleTable component

--- src/components/pages/Tables/SingleTable.js
+++ src/components/pages/Tables/SingleTable.js
@@ -1,33 +1,100 @@
-import { useSelector } from 'react-redux';
-import { getTableById } from '../../../redux/tablesRedux';
-import { useParams, Link } from 'react-router-dom';
-import { Row, Col, Container, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { editTableRequest, getTableById } from '../../../redux/tablesRedux';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Row, Col, Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 const SingleTable = () => {
   const { id } = useParams();
-  const tableId = useSelector((state) => getTableById(state, id));
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const getTable = useSelector((state) => getTableById(state, id));
 
+  const [table] = useState(getTable.id);
+  const [status, setStatus] = useState(getTable.status);
+  const [peopleAmount, setPeopleAmount] = useState(getTable.peopleAmount);
+  const [maxPeopleAmount, setMaxPeopleAmount] = useState(
+    getTable.maxPeopleAmount
+  );
+  const [bill, setBill] = useState(getTable.bill);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      editTableRequest({
+        table,
+        peopleAmount,
+        maxPeopleAmount,
+        status,
+        bill,
+        id,
+      })
+    );
+    navigate('/');
+  };
   return (
     <>
-      <h1>Table {tableId.id}</h1>
-      <Container>
-        <Row className="justify-content-center mt-4">
-          <Col>
-            <p>
-              <b>Status: </b> {tableId.status}
-            </p>
-            <p>
-              <b>People:</b> {tableId.peopleAmount} / {tableId.maxPeopleAmount}
-            </p>
-            <p>
-              <b>Bill:</b> {tableId.bill}
-            </p>
-            <Link to={'/'}>
-              <Button variant="primary">Update</Button>
-            </Link>
+      <h2>Table {table}</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label as="legend" column sm={1}>
+            <strong>Status:</strong>
+          </Form.Label>
+          <Col sm={3}>
+            <Form.Select
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+            >
+              <option value="busy">Busy</option>
+              <option value="free">Free</option>
+              <option value="cleaning">Cleaning</option>
+              <option value="reserved">Reserved</option>
+            </Form.Select>
           </Col>
-        </Row>
-      </Container>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column sm={1}>
+            <strong>People:</strong>
+          </Form.Label>
+          <Col sm={1}>
+            <Form.Control
+              type="text"
+              value={peopleAmount}
+              onChange={(e) => setPeopleAmount(e.target.value)}
+            />
+          </Col>
+          /
+          <Col sm={1}>
+            <Form.Control
+              type="text"
+              value={maxPeopleAmount}
+              onChange={(e) => setMaxPeopleAmount(e.target.value)}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column sm={1}>
+            <strong>Bill:</strong>
+          </Form.Label>
+          <Col sm={2}>
+            <Row>
+              <Col sm={1}>$</Col>
+              <Col sm={6}>
+                <Form.Control
+                  type="text"
+                  value={bill}
+                  onChange={(e) => setBill(e.target.value)}
+                />
+              </Col>
+            </Row>
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-2">
+          <Col>
+            <Button type="submit">Update</Button>
+          </Col>
+        </Form.Group>
+      </Form>
     </>
   );
 };
