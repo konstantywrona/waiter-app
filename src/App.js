d76a8ddcb95522d5dfbd914bfc1f415fcc0d0124
Konstanty Wrona <koswro@gmail.com>
2023-04-13 01:44:44 +0200

    Change name Table to SingleTable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Home from './components/pages/Home/Home';
-import Tables from './components/pages/Tables/Tables';
+import SingleTable from './components/pages/Tables/SingleTable';
 import NotFound from './components/pages/NotFound/NotFound';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
@@ -13,7 +13,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="table/:id" element={<Tables />} />
+          <Route path="table/:id" element={<SingleTable />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
--- src/components/pages/Tables/Tables.js
+++
@@ -1,5 +0,0 @@
-const Tables = () => {
-  return <h1>Tables</h1>;
-};
-
-export default Tables;
