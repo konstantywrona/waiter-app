22ecb794cd8fa54f917b5326bd44026a4f423e82
Konstanty Wrona <koswro@gmail.com>
2023-04-13 01:41:55 +0200

    Add selector and action creator

--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -1,11 +1,19 @@
+import shortid from 'shortid';
+
 //selectors
+export const getAllTables = (state) => state.tables;
 
 // actions
 const createActionName = (actionName) => `app/tables/${actionName}`;
+const EDIT_TABLE = createActionName('EDIT_TABLE');
 
 // action creators
+export const editTable = (payload) => ({ type: EDIT_TABLE, payload });
+
 const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
+    case EDIT_TABLE:
+      return [...statePart, { ...action.payload, id: shortid() }];
     default:
       return statePart;
   }
