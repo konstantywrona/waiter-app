12a40f1f71216b0ee0b89344fadb5cdfacbf7d24
Konstanty Wrona <koswro@gmail.com>
2023-04-13 01:34:14 +0200

    Add navbar styles

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,5 +1,24 @@
+import { Navbar, NavbarBrand, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
 const NavBar = () => {
-  return <h2>NavBar</h2>;
+  return (
+    <Navbar
+      bg="primary"
+      variant="dark"
+      expand="lg"
+      className="mt-4 mb-4 rounded d-flex justify-content-between"
+    >
+      <NavbarBrand className="justify-content-start px-3" href="/">
+        Waiter.app
+      </NavbarBrand>
+      <Nav className="flex-sm-column flex-md-row px-3">
+        <Nav.Link as={NavLink} to="/">
+          Home
+        </Nav.Link>
+      </Nav>
+    </Navbar>
+  );
 };
 
 export default NavBar;
