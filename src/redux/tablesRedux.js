a25b74d802bca45a50d30534bef07be9c3c058c7
Konstanty Wrona <koswro@gmail.com>
2023-04-13 01:46:17 +0200

    Change name of the action update

--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -7,14 +7,14 @@ export const getTableById = ({ tables }, id) =>
 
 // actions
 const createActionName = (actionName) => `app/tables/${actionName}`;
-const EDIT_TABLE = createActionName('EDIT_TABLE');
+const UPDATE_TABLE = createActionName('UPDATE_TABLE');
 
 // action creators
-export const editTable = (payload) => ({ type: EDIT_TABLE, payload });
+export const editTable = (payload) => ({ type: UPDATE_TABLE, payload });
 
 const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
-    case EDIT_TABLE:
+    case UPDATE_TABLE:
       return [...statePart, { ...action.payload, id: shortid() }];
     default:
       return statePart;
