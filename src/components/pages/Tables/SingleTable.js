16d1441ed1388fae54d5b27b3229546299c41e27
Konstanty Wrona <koswro@gmail.com>
2023-04-13 01:47:47 +0200

    Change selector

--- src/components/pages/Tables/SingleTable.js
+++ src/components/pages/Tables/SingleTable.js
@@ -1,12 +1,35 @@
 import { useSelector } from 'react-redux';
 import { getTableById } from '../../../redux/tablesRedux';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { Row, Col, Container, Button } from 'react-bootstrap';
 
 const SingleTable = () => {
   const { id } = useParams();
-  const tableData = useSelector((state) => getTableById(state, id));
+  const tableId = useSelector((state) => getTableById(state, id));
 
-  return <h1>Table {tableData.id}</h1>;
+  return (
+    <>
+      <h1>Table {tableId.id}</h1>
+      <Container>
+        <Row className="justify-content-center mt-4">
+          <Col>
+            <p>
+              <b>Status: </b> {tableId.status}
+            </p>
+            <p>
+              <b>People:</b> {tableId.peopleAmount} / {tableId.maxPeopleAmount}
+            </p>
+            <p>
+              <b>Bill:</b> {tableId.bill}
+            </p>
+            <Link to={'/'}>
+              <Button variant="primary">Update</Button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default SingleTable;
