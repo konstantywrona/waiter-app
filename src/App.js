1b538a5fdc7dfe6185b8f58ea9873a7892fcb358
Konstanty Wrona <koswro@gmail.com>
2023-04-13 01:30:11 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Home from './components/pages/Home/Home';
+import Tables from './components/pages/Tables/Tables';
+import NotFound from './components/pages/NotFound/NotFound';
+
 const App = () => {
-  return <div className="App">Hello world</div>;
+  return (
+    <main>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="table/:id" element={<Tables />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </main>
+  );
 };
 
 export default App;
